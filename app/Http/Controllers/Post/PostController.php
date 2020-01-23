32a6560990453f92edd97659002c7052da21006a
meister68 <53040397+meister68@users.noreply.github.com>
2020-01-23 17:16:21 +0800

    Update PostController.php

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Post;
+namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Model\Post;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -17,16 +17,18 @@ class PostController extends Controller
         // ]);
 
         // Post::create($request->all());
-        return redirect('/home');
+        //return redirect('/home');
+        return 'create post page';
     }
 
     public function editPost($post_id)  
     {   
         $post = Post::find($post_id);
-        return redirect('/home');
+        //return redirect('/home');
+        return $post_id;
     }
 
-    public function updatePost(Request $request, $post_id)
+    public function update(Request $request, $post_id)
     {
         Post::updatePost($request->title, $request->description, $post_id);
         return redirect('/home');
@@ -34,7 +36,7 @@ class PostController extends Controller
 
     public function removePost($post_id)
     {
-        Post::delete($post_id);
+        Post::deletePost($post_id);
         return  redirect('/home');
     }
 }
