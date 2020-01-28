7166322b1ec75afa321da336acf8ee227887f489
2ndyrGroupA <2ndyrGroupA@pn.local>
2020-01-28 10:00:54 +0800

    search for completion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        <form class="form-inline my-2 my-lg-0" >
+                        <form class="form-inline my-2 my-lg-0" method="GET" action="{{ route('search') }}">
                         @csrf
                             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
